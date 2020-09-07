406e3ab54bf331731107658f801ea85393b9f46f
manuelpapa <manulito@de-vio.com>
2020-09-07 16:03:30 +0200

    Fix server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const jsonServer = require("json-server");
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3001;
 
@@ -9,6 +12,8 @@ app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
 );
+app.use(middlewares);
+app.use(router);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
