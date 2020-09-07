f36a6e30e3f927672174becff7b02cb49067ed47
manuelpapa <manulito@de-vio.com>
2020-09-07 15:32:25 +0200

    Add concurrently and create dev script

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const jsonServer = require("json-server");
-// const router = jsonServer.router("db.json");
-// const middlewares = jsonServer.defaults();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
 
 const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "client/build")));
-// app.use(middlewares);
-// app.use(router);
+app.use(middlewares);
+app.use(router);
 app.use(
   "/storybook",
   express.static(path.join(__dirname, "client/storybook-static"))
