3f0f636c8aed588c47881d434533b5502027acd0
manuelpapa <manulito@de-vio.com>
2020-09-21 14:05:30 +0200

    Add TimeRangePicker

--- client/src/components/DateTimePicker.js
+++ client/src/components/DateTimePicker.js
@@ -1,28 +1,26 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { TimePicker } from "antd";
+import "antd/dist/antd.css";
 
 export function DateTimePicker() {
   const [selectedDate, setSelectedDate] = useState(null);
-  const [startDate, setStartDate] = useState(new Date());
+  const { RangePicker } = TimePicker;
 
   return (
-    <div>
-      <DatePicker
-        selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
-        dateFormat="dd.MM.yyyy"
-        isClearable="true"
-      />
-      <DatePicker
-        selected={startDate}
-        onChange={(date) => setStartDate(date)}
-        showTimeSelect
-        showTimeSelectOnly
-        timeIntervals={15}
-        timeCaption="Zeit"
-        dateFormat="H:mm"
-      />
-    </div>
+    <>
+      <div>
+        <DatePicker
+          selected={selectedDate}
+          onChange={(date) => setSelectedDate(date)}
+          dateFormat="dd.MM.yyyy"
+          isClearable="true"
+        />
+      </div>
+      <div>
+        <RangePicker format="HH:mm" minuteStep={15} />
+      </div>
+    </>
   );
 }
