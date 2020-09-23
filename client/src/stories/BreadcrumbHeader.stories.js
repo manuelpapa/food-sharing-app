8cc354db0bc85e4e53937d4960aa0dd3a831ee73
manuelpapa <manulito@de-vio.com>
2020-09-23 17:39:32 +0200

    Add BreacrumbHeader to storybook

--- client/src/stories/BreadcrumbHeader.stories.js
+++ client/src/stories/BreadcrumbHeader.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import BreadcrumbHeader from "../components/BreadcrumbHeader";
+import { BreadcrumbHeader } from "../components/BreadcrumbHeader";
 
 export default {
   title: "BreadcrumbHeader",
