98e9d5f2a6fa5867fa686c68e63d9172a75545a4
manuelpapa <manulito@de-vio.com>
2020-09-24 22:34:43 +0200

    Prepare server.js for mongoose

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const { MongoClient } = require("mongodb");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const createUsersRouter = require("./routes/users");
 const createOffersRouter = require("./routes/offers");
+const authRoute = require("./routes/auth");
+const offerRoute = require("./routes/offer");
 
 const client = new MongoClient(process.env.MONGO_URI, {
   useUnifiedTopology: true,
 });
+
+// Connect to DB with Mongoose
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true }, () =>
+  console.log("connected to db!")
+);
 
 const app = express();
 
@@ -27,8 +35,13 @@ async function main() {
     next();
   });
 
+  // Old Routes
   app.use("/api/users", createUsersRouter(database));
   app.use("/api/offers", createOffersRouter(database));
+
+  // New Routes
+  app.use("/api/user", authRoute);
+  app.use("/api/offer", offerRoute);
 
   app.get("/", (request, response) => {
     response.sendFile(__dirname + "/index.html");
