3550274f73f9750bc79f7f5c3af826004c7b2816
manuelpapa <manulito@de-vio.com>
2020-09-24 22:35:03 +0200

    Add user schema

---
+++ model/User.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    min: 6,
+    max: 255,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 255,
+    min: 6,
+  },
+  password: {
+    type: String,
+    required: true,
+    max: 1024,
+    min: 6,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
