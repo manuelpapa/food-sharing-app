c20421a42c63cc8cb66b5ecefb353b854d838772
manuelpapa <manulito@de-vio.com>
2020-09-25 14:50:42 +0200

    Login user via api

--- routes/auth.js
+++ routes/auth.js
@@ -35,23 +35,31 @@ router.post("/register", async (req, res) => {
 
 // LOGIN
 router.post("/login", async (req, res) => {
-  // Validate the datea, before logging in the user
-  const { error } = loginValidation(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
+  try {
+    const { error } = loginValidation(req.body);
+    if (error) {
+      return res.status(400).send(error.details[0].message);
+    }
+    const { email, password } = req.body;
+
+    // Checking if the email is in db
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).send("Email doesn't exist");
+
+    // Check if password is correct
+    const validPass = await bcrypt.compare(password, user.password);
+    if (!validPass) return res.status(400).send("Invalid password");
+
+    // Create and sign a Token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+    // Return JWT to Client
+    res.setHeader("Set-Cookie", `auth-token=${token};path=/;Max-Age=604800`);
+    res.json(token);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Internal server error");
   }
-  // Checking if the user is in db
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("Email doesn't exist");
-
-  // Check if password is correct
-  const validPass = await bcrypt.compare(req.body.password, user.password);
-  if (!validPass) return res.status(400).send("Invalid password");
-
-  // Create and sign a Token
-  const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
-
-  res.header("auth-token", token).send(token);
 });
 
 module.exports = router;
