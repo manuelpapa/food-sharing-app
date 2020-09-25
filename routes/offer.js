eec4e2424a9ec4b4a3d06387991f1350ddc133b3
manuelpapa <manulito@de-vio.com>
2020-09-25 17:58:57 +0200

    Add POST request for offer

--- routes/offer.js
+++ routes/offer.js
@@ -1,4 +1,5 @@
 const User = require("../model/User");
+const Offer = require("../model/Offer");
 const router = require("express").Router();
 const verify = require("./verifyToken");
 
@@ -6,6 +7,31 @@ router.get("/", verify, async (req, res) => {
   // Now we have the _id in req.user and can find the User in the db
   const user = await User.findById(req.user._id);
   res.send(user);
+});
+
+router.post("/", verify, async (req, res) => {
+  try {
+    const offer = new Offer({
+      title: req.body.title,
+      category: req.body.category,
+      date: req.body.date,
+      start_time: req.body.start_time,
+      end_time: req.body.end_time,
+      location: req.body.location,
+      tags: req.body.tags,
+      reserved_by: req.body.reserved_by,
+      created_by: req.body.created_by,
+    });
+
+    try {
+      await offer.save();
+      res.send({ offer: offer._id });
+    } catch (error) {
+      res.status(400).send(error);
+    }
+  } catch (error) {
+    res.status(500).send("Internal server error");
+  }
 });
 
 module.exports = router;
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
 
 module.exports = function (req, res, next) {
-  const token = req.header("auth-token");
+  const cookie = req.header("Cookie").split("=");
+  const token = cookie[1];
+
   if (!token) return res.status(401).send("Access Denied");
 
   try {
