ba6b99718cdad9d0addfcd6b17d17c815923e12f
manuelpapa <manulito@de-vio.com>
2020-09-26 14:32:43 +0200

    Fix deprecation bug in mongoose

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ async function main() {
   await mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useFindAndModify: false,
   });
 
   app.use(bodyParser.json());
