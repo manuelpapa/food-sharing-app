fc9ea3c2941e53ee272621de4eba74ec5b6d41cc
manuelpapa <manulito@de-vio.com>
2020-09-26 09:08:11 +0200

    Fix MutationObserver issue

--- client/src/setupTests.js
+++ client/src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
+import jsdom from "jsdom";
+const dom = new jsdom.JSDOM("<!doctype html><html><body></body></html>");
+global.window = dom.window;
+global.document = dom.window.document;
+global.navigator = dom.window.navigator;
+global.Node = dom.window.Node;
+require("mutationobserver-shim");
+global.MutationObserver = global.window.MutationObserver;
