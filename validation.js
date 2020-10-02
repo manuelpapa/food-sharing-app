b4728f52dd00c1b8446b80e97b70cb5f9a9a1676
manuelpapa <manulito@de-vio.com>
2020-10-02 13:13:15 +0200

    Change name length in registering

--- validation.js
+++ validation.js
@@ -2,7 +2,7 @@ const Joi = require("@hapi/joi");
 
 const registerValidation = (data) => {
   const schema = Joi.object({
-    name: Joi.string().min(6).required(),
+    name: Joi.string().min(3).required(),
     email: Joi.string().min(6).required().email(),
     password: Joi.string().min(6).required(),
   });
